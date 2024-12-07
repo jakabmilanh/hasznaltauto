e5ed4cf26a56ba98b44b2a5e0a19c8556d8b0453
jakabmilanh <77169556+jakabmilanh@users.noreply.github.com>
2024-12-07 10:07:03 +0100

    s

--- laravel/resources/views/pages/main.blade.php
+++ laravel/resources/views/pages/main.blade.php
@@ -17,6 +17,14 @@
             </div>
         @endif
 
+        <!-- Logout Button -->
+        <div class="mb-3 text-end">
+            <form action="{{ route('logout') }}" method="POST">
+                @csrf
+                <button type="submit" class="btn btn-danger">Logout</button>
+            </form>
+        </div>
+
         <table class="table table-bordered">
             <thead>
                 <tr>
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -8,16 +8,14 @@ Route::get('/', function () {
     return view('auth/login');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::get('/cars', [CarController::class, 'index'])->name('cars.index');
+    Route::get('/dashboard', [CarController::class, 'index'])->name('cars.index');
     
     Route::get('/new-car', [CarController::class, 'create'])->name('cars.create');
     Route::post('/cars', [CarController::class, 'store'])->name('cars.store');

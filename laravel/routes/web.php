9fea9b810d57a58cb5a4754afdec98a511cf4ee3
jakabmilanh <77169556+jakabmilanh@users.noreply.github.com>
2024-12-07 10:42:53 +0100

    Update web.php

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -8,17 +8,14 @@ Route::get('/', function () {
     return view('auth/login');
 });
 
-Route::middleware(['auth'])->group(function () {
-    Route::get('/new-car', [CarController::class, 'create'])->name('cars.create');
-    Route::post('/cars', [CarController::class, 'store'])->name('cars.store');
-});
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::get('/dashboard', [CarController::class, 'index'])->name('cars.index');
+    Route::get('/new-car', [CarController::class, 'create'])->name('cars.create');
+    Route::post('/cars', [CarController::class, 'store'])->name('cars.store');
 
 
 
